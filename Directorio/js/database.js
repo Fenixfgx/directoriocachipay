049ae40ead53366ff78303aae2dfd2133d6b2d56
Fenixfgx <72283013+Fenixfgx@users.noreply.github.com>
2022-11-08 16:48:58 -0500

    Add files via upload

---
+++ Directorio/js/database.js
@@ -0,0 +1,6 @@
+[
+    {"claves":"David Zapata, 1069585617","description":"12 Mb","link":"http:\/\/fenixfgx.github.io/Break.net/Breaknet/Facturas/a1/a1.pdf","title":"David Zapata"},
+    
+    {"claves":"Angel Barragan, 1000000000","description":"15 Mb","link":"http:\/\/fenixfgx.github.io/Break.net/Breaknet/Facturas/a2/a1.pdf","title":"Angel Barragan"}
+
+]
