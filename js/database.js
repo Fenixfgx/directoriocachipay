c5ecae66b54a19cb0dc689bc382f78786a8f98f9
Fenixfgx <72283013+Fenixfgx@users.noreply.github.com>
2023-01-03 12:18:08 -0500

    Add files via upload

---
+++ js/database.js
@@ -0,0 +1,23 @@
+﻿[{"claves":"Break.net, Break, Angel Barragan, Computadores, Computadoras, Breakstation, Internet, Provedor de Internet, Proveedor, Internet Rural, Internet para el campo, finca, campo, servicio de internet","description":"Proveedor de Servicio de Internet para el Hogar Urbano y Rural","link":"http:\/\/www.break.net","title":"Break.net"}
+,{"claves":"Telecomtrol, la mona, la monita, donde Diana, Papeleria, internet, fotocopias, escaner, recargas, tareas, trabajos, laminación, laminar, revistas, , ","description":"Papeleria, Internet, Recargas, Fotocopias y mucho mas...","link":"/telecomtrol.html","title":"Telecomtrol"}
+,{"claves":"Rapizzpollo, Rapispollo, almuerzo, carnes, filetes, alimentos, almuerzos, pollo asado, comida, alitas de pollo, pollo broster, pollo broaster, Pollo","description":"Asadero de pollo","link":"/Rapizzpollo.html","title":"Rapizzpollo"}
+,{"claves":"Las Brasas de Cachipay, Brasas, almuerzo, carnes, filetes, alimentos, almuerzos, comida, pollo asado, alitas de pollo, pollo broster, pollo broaster, Pollo","description":"Asadero de pollo","link":"/brasasdecachipay.html","title":"Las Brasas de Cachipay"}
+,{"claves":"Restaurante San Angelo, San Angelo, carnes, filetes almuerzo, alimentos, Restaurante, restaurante italiano, restaurante familiar, pasta, espagueti, espaghetti, lasagna, lasaña, comida, pollo, alitas de pollo, pollo broster, Pollo","description":"Restaurante Italiano","link":"/sanangelo.html","title":"San Angelo"}
+,{"claves":"Restaurante Los Balcones, Los balcones, carnes, filetes, almuerzo, alimentos, Restaurante, restaurante italiano, restaurante familiar, comida, pollo, alitas de pollo, pollo broaster, Sopa, Pollo","description":"Restaurante Familiar","link":"/losbalcones.html","title":"Los Balcones"}
+,{"claves":"Cootrasan, Camionetas, taxis, travel, viajar, transporte, paqueteria, mensajeria, viajes, bogota, cartagenita, las camionetas","description":"Viaje Seguro","link":"/Cootrasan.html","title":"Cootrasan"}
+,{"claves":"Expreso el Sol, Buses, transporte, viajar, tiquetes, viajes, paqueteria, travel","description":"Linea de autobuses","link":"/expresoelsol.html","title":"Expreso el Sol"}
+,{"claves":"Ferreteria Don Oscar, Oscar, Ferretería, ladrillos, perfiles, tornillos, varillas, cemento,","description":"Ferreteria y Construccion","link":"/ferreteriadonoscar.html","title":"Ferreteria Don Oscar"}
+,{"claves":"Veterinaria NN, mascotas, animales,alimento para gato, alimento para perro, concentrados, peces, perros, gatos, caballos medicina, ","description":"Al cuidado de las Mascotas","link":"/veterinaria.html","title":"Veterinaria NN"}
+,{"claves":"Contegral, Cointegral, mascotas, alimento para gato, alimento para perro, animales, concentrados, peces, medicina, ","description":"Concentrado para Animales","link":"/contegral.html","title":"Contegral"}
+,{"claves":"Fitness Club, Club, Gimnasios, GYM, masajes, relajacion, bailes, danzas","description":"Danzas, Gimnasio, Masajes y mucho mas..","link":"/fitnessclub.html","title":"Fitness Club"}
+,{"claves":"Funarde, Bailes, Tekondo, Taekwondo, Ballet, Balet, Yamile, Fundacion, Donaciones, Apadrinar, niños, niñas","description":"Fundación de Arte y Cultura, Apadrina un niño..","link":" FUNARDE/index.html","title":"FUNARDE"}
+,{"claves":"Turismo, Viajes, Vuelos, Vacaciones, ONvacation, Esperanza, Servicios","description":"Viaja a los mejores destinos y al mejor precio","link":"/onvacationcachipay.html","title":"ON VACATION CACHIPAY"}
+,{"claves":"Partido, Politica, Colombia Consciente, Consciente, Danny","description":"Partido Politico","link":"https://www.partidocolombiaconsciente.com/","title":"Partido Politico Colombia Consciente"}
+,{"claves":"App, Aplicacion, Cachipay, Directorio Cachipay, DC","description":"Gestion y Contacto","link":" Directorio/DC.html","title":"Directorio Cachipay"}
+,{"claves":"Policia, tombos, Nacional, Fuerza Publica, 123, Policía, Cuadrante","description":"Fuerza Publica","link":" Directorio/Policia-Nacional.html","title":"Policía Cachipay"}
+
+
+
+
+
+]
