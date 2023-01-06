5dae743916306b01f6bf86e8e02df587474f8e92
Fenixfgx <72283013+Fenixfgx@users.noreply.github.com>
2023-01-05 23:22:41 -0500

    Update database.js

--- js/database.js
+++ js/database.js
@@ -1,7 +1,9 @@
-{
+[{
   "claves": "Break.net, Break, Angel Barragan, Computadores, Computadoras, Breakstation, Internet, Provedor de Internet, Proveedor, Internet Rural, Internet para el campo, finca, campo, servicio de internet",
   "description": "Proveedor de Servicio de Internet para el Hogar Urbano y Rural",
   "link": "http://www.break.net",
   "title": "Break.net",
   "image": "https://conceptodefinicion.de/wp-content/uploads/2014/05/Imagen-2.jpg"
 }
+
+ ]
