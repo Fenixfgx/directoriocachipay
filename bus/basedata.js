dc3e9c49ae54a484361f8cb6d49ecdcb0162f6a5
Fenixfgx <72283013+Fenixfgx@users.noreply.github.com>
2023-02-22 14:35:33 -0500

    Add files via upload

--- bus/basedata.js
+++ bus/basedata.js
@@ -1,8 +1,8 @@
 const database = [
-  { title: "Playa", image: "http://4.bp.blogspot.com/-uVaD4a4UMlw/V3EjAw_NzGI/AAAAAAAAGB8/LfjFTMHFQD0am8rkCp2YQB2V8Yg2KETkwCK4B/s1600/1449789343-620d343f58f5e93c9de1d5b3f5b966de.jpeg", description: "Imagen de una hermosa playa con palmeras", phone: "3046234592" },
-  { title: "Montañas", image: "https://i.pinimg.com/236x/0d/03/0b/0d030bccc849ba15fe764b8ec244f4d9.jpg", description: "Imagen de unas impresionantes montañas nevadas", phone: "3046234592" },
-  { title: "Ciudad", image: "https://w0.peakpx.com/wallpaper/805/457/HD-wallpaper-animals-hot-anime-girl-sexy-anime-girl-anime-girl-anime.jpg", description: "Imagen de una animada ciudad de noche", phone: "3046234592" },
-  { title: "Bosque", image: "https://i.pinimg.com/736x/02/0f/50/020f50c861604df0f0556b7f7335e22a.jpg", description: "Imagen de un frondoso bosque con un arroyo", phone: "3046234592" },
-  { title: "Paramo", image: "https://1.bp.blogspot.com/-nFIv5AWV5RI/XyTUCVyOa9I/AAAAAAAADm8/wIEXbiOZW18WnWsHHLdKAjtAr7G_2V0hQCNcBGAsYHQ/w451-h640/Anime-Girl-Wallpaper-HD-Android-084.jpg", description: "Imagen de un frondoso paramo con un arroyo", phone: "3046234592" },  
+  { title: "Playa", image: "imagenes/c.jpeg", description: "Imagen de una hermosa playa con palmeras", phone: "3046234592" },
+  { title: "Montañas", image: "imagenes/a.jpg", description: "Imagen de unas impresionantes montañas nevadas", phone: "3046234592" },
+  { title: "Ciudad", image: "imagenes/d.jpg", description: "Imagen de una animada ciudad de noche", phone: "3046234592" },
+  { title: "Bosque", image: "imagenes/e.jpg", description: "Imagen de un frondoso bosque con un arroyo", phone: "3046234592" },
+  { title: "Paramo", image: "imagenes/b.jpg", description: "Imagen de un frondoso paramo con un arroyo", phone: "3046234592" },  
     
 ];
