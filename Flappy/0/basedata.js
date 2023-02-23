9a4b544500bf0afbdd0effb46ab52381c8da33eb
Fenixfgx <72283013+Fenixfgx@users.noreply.github.com>
2023-02-23 02:54:03 -0500

    Add files via upload

---
+++ Flappy/0/basedata.js
@@ -0,0 +1,4 @@
+﻿const database = [  { title: "Policia", image: "imagenes/pol.jpg", description: "Policia Nacional", phone: "3046234592", keywords: ["policia", "fuerza publica"] },
+{ title: "Restaurante", image: "imagenes/res.png", description: "Restaurante Gourmet", phone: "3046234592", keywords: ["pasta", "pizza", "spaguetti", "papas", "pollo"] },
+  
+];
