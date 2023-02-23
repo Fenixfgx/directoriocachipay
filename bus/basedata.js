6ed91b572eec6180b26724afb739c3572aa54f6d
Fenixfgx <72283013+Fenixfgx@users.noreply.github.com>
2023-02-23 00:16:23 -0500

    Add files via upload

--- bus/basedata.js
+++ bus/basedata.js
@@ -1,8 +1,6 @@
-const database = [
-  { title: "Playa", image: "imagenes/c.jpeg", description: "Imagen de una hermosa playa con palmeras", phone: "3046234592" },
-  { title: "Montañas", image: "imagenes/a.jpg", description: "Imagen de unas impresionantes montañas nevadas", phone: "3046234592" },
-  { title: "Ciudad", image: "imagenes/d.jpg", description: "Imagen de una animada ciudad de noche", phone: "3046234592" },
-  { title: "Bosque", image: "imagenes/e.jpg", description: "Imagen de un frondoso bosque con un arroyo", phone: "3046234592" },
-  { title: "Paramo", image: "imagenes/b.jpg", description: "Imagen de un frondoso paramo con un arroyo", phone: "3046234592"},  
-    
+const database = [  { title: "Playa", image: "imagenes/c.jpeg", description: "Imagen de una hermosa playa con palmeras", phone: "3046234592", keywords: ["playa", "palmeras", "mar", "arena"] },
+  { title: "Montañas", image: "imagenes/a.jpg", description: "Imagen de unas impresionantes montañas nevadas", phone: "3046234592", keywords: ["montañas", "nevado", "nieve", "paisaje"] },
+  { title: "Ciudad", image: "imagenes/d.jpg", description: "Imagen de una animada ciudad de noche", phone: "3046234592", keywords: ["ciudad", "noche", "edificios", "luces"] },
+  { title: "Bosque", image: "imagenes/e.jpg", description: "Imagen de un frondoso bosque con un arroyo", phone: "3046234592", keywords: ["bosque", "arboles", "naturaleza", "agua"] },
+  { title: "Paramo", image: "imagenes/b.jpg", description: "Imagen de un frondoso paramo con un arroyo", phone: "3046234592", keywords: ["paramo", "arroyo", "naturaleza", "agua"] }
 ];
